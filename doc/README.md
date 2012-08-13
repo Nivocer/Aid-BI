4dfcf68fed5c670ad0c36e76cd7149aebe7c3fe0
Rolf Kleef <r.kleef@nivocer.com>
2012-08-13 18:53:58 +0200

    Couchapp intended for "embedded documentation" (currently: generated wireframes)

---
+++ doc/README.md
@@ -0,0 +1,9 @@
+## Generated CouchApp for embedded documentation
+
+This CouchApp directory is intended to contain (generated) online documentation.
+
+You can push this app to your CouchDB database to have access to
+the documentation via a URL like:
+
+	http://127.0.0.1:5984/aid-bi/_design/doc/index.html
+
---
+++ doc/_attachments/wireframes/CheckOrganisationProfile_files/story.js
@@ -0,0 +1,28 @@
+var story = {
+ "title": "CheckOrganisationProfile",
+ "pages": [
+  {
+   "title": "select-organisation",
+   "height": 666,
+   "width": 1016,
+   "image": "select-organisation.png",
+   "links": [{
+    "page": 1,
+    "rect": [
+     155,
+     448,
+     832,
+     555
+    ]
+   }]
+  },
+  {
+   "title": "organisation",
+   "height": 891,
+   "width": 1016,
+   "image": "organisation.png",
+   "links": []
+  }
+ ],
+ "highlightLinks": false
+}
